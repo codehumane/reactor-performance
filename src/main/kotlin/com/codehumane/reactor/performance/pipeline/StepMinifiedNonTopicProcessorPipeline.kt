652aa89ab48ad177725fc31988cabd1886a0d294
codehumane <codehumane@gmail.com>
2019-06-27 09:53:47 +0900

    StepMinifiedNonTopicProcessor에서 중간 단계의 스레드 갯수 늘림 (32 -> 128) -> 많이 나아짐!!!

--- src/main/kotlin/com/codehumane/reactor/performance/pipeline/StepMinifiedNonTopicProcessorPipeline.kt
+++ src/main/kotlin/com/codehumane/reactor/performance/pipeline/StepMinifiedNonTopicProcessorPipeline.kt
@@ -65,13 +65,60 @@ pipeline_final_12 tps: 444.44444444444446, detail: 447, 443, 444, 442, 446, 445,
 pipeline_final_13 tps: 444.44444444444446, detail: 448, 441, 446, 442, 445, 447, 442, 447, 442
 pipeline_final_14 tps: 444.55555555555554, detail: 447, 445, 444, 444, 444, 446, 443, 447, 441
 pipeline_final_15 tps: 444.6666666666667, detail: 449, 442, 447, 440, 446, 445, 442, 447, 444
+
+
+ *******************
+좀 더 개선해서 통합된 단계의 스레드 갯수를 늘리니 더 개선됨!!!
+ *******************
+
+<시작할 무렵>
+start tps: 9550.333333333334, detail: 9305, 10316, 9733, 8891, 9304, 9396, 7698, 10871, 10439
+step1 tps: 9550.333333333334, detail: 9304, 10317, 9733, 8891, 9304, 9396, 7698, 10871, 10439
+step2 tps: 9550.111111111111, detail: 9314, 10308, 9741, 8887, 9301, 9413, 7677, 10867, 10443
+pipeline_final_0 tps: 596.5555555555555, detail: 579, 642, 613, 555, 580, 592, 477, 679, 652
+pipeline_final_1 tps: 596.7777777777778, detail: 582, 644, 613, 552, 581, 593, 472, 681, 653
+pipeline_final_2 tps: 596.7777777777778, detail: 585, 637, 615, 553, 584, 590, 475, 681, 651
+pipeline_final_3 tps: 596.3333333333334, detail: 588, 634, 618, 555, 576, 595, 477, 677, 647
+pipeline_final_4 tps: 596.4444444444445, detail: 583, 642, 613, 555, 582, 590, 477, 678, 648
+pipeline_final_5 tps: 596.7777777777778, detail: 584, 645, 608, 558, 580, 591, 475, 679, 651
+pipeline_final_6 tps: 597.1111111111111, detail: 585, 643, 612, 555, 579, 592, 472, 684, 652
+pipeline_final_7 tps: 596.7777777777778, detail: 583, 644, 610, 556, 579, 592, 477, 676, 654
+pipeline_final_8 tps: 597.0, detail: 582, 639, 616, 554, 581, 591, 474, 680, 656
+pipeline_final_9 tps: 597.3333333333334, detail: 586, 641, 614, 555, 580, 593, 476, 678, 653
+pipeline_final_10 tps: 596.7777777777778, detail: 586, 642, 612, 554, 579, 593, 474, 680, 651
+pipeline_final_11 tps: 596.8888888888889, detail: 583, 644, 612, 556, 581, 590, 477, 680, 649
+pipeline_final_12 tps: 596.6666666666666, detail: 584, 642, 611, 557, 578, 593, 478, 674, 653
+pipeline_final_13 tps: 596.7777777777778, detail: 583, 642, 611, 555, 581, 592, 474, 676, 657
+pipeline_final_14 tps: 596.8888888888889, detail: 586, 642, 611, 554, 582, 590, 479, 678, 650
+pipeline_final_15 tps: 597.1111111111111, detail: 583, 644, 609, 553, 583, 591, 476, 677, 658
+
+<끝날 무렵 = 사실은 두 번째 만에 끝남>
+tart tps: 10407.444444444445, detail: 10231, 10441, 10310, 10651, 10417, 10512, 10416, 10316, 10373
+step1 tps: 10407.444444444445, detail: 10230, 10442, 10310, 10651, 10417, 10512, 10416, 10316, 10373
+step2 tps: 10407.777777777777, detail: 10235, 10436, 10310, 10648, 10419, 10518, 10415, 10313, 10376
+pipeline_final_0 tps: 650.8888888888889, detail: 645, 648, 644, 669, 649, 656, 650, 649, 648
+pipeline_final_1 tps: 650.2222222222222, detail: 634, 652, 650, 661, 654, 656, 654, 644, 647
+pipeline_final_2 tps: 650.5555555555555, detail: 642, 650, 649, 662, 652, 658, 652, 645, 645
+pipeline_final_3 tps: 650.1111111111111, detail: 639, 651, 646, 662, 655, 657, 650, 647, 644
+pipeline_final_4 tps: 650.4444444444445, detail: 642, 653, 644, 666, 650, 655, 652, 645, 647
+pipeline_final_5 tps: 651.0, detail: 642, 653, 647, 662, 652, 659, 652, 640, 652
+pipeline_final_6 tps: 650.4444444444445, detail: 641, 647, 651, 662, 655, 655, 652, 646, 645
+pipeline_final_7 tps: 650.3333333333334, detail: 643, 650, 647, 664, 654, 654, 654, 645, 642
+pipeline_final_8 tps: 650.1111111111111, detail: 637, 654, 646, 664, 653, 655, 653, 642, 647
+pipeline_final_9 tps: 650.4444444444445, detail: 638, 653, 647, 662, 655, 655, 648, 650, 646
+pipeline_final_10 tps: 650.3333333333334, detail: 639, 650, 650, 662, 651, 660, 650, 644, 647
+pipeline_final_11 tps: 649.8888888888889, detail: 640, 648, 648, 666, 648, 661, 648, 648, 642
+pipeline_final_12 tps: 650.5555555555555, detail: 640, 648, 651, 667, 650, 656, 648, 650, 645
+pipeline_final_13 tps: 650.6666666666666, detail: 641, 653, 644, 665, 649, 660, 648, 649, 647
+pipeline_final_14 tps: 651.5555555555555, detail: 650, 649, 647, 661, 656, 658, 648, 645, 650
+pipeline_final_15 tps: 650.0, detail: 638, 651, 646, 667, 649, 658, 650, 644, 647
  */
 @Service
 class StepMinifiedNonTopicProcessorPipeline(private val meterRegistry: PrometheusMeterRegistry) {
 
     private val log = LoggerFactory.getLogger(StepMinifiedNonTopicProcessorPipeline::class.java)
 
-    private val intermediateTransformThreadCoreSize = 32
+    private val intermediateTransformThreadCoreSize = 128
     private val finalItemThreadCoreSize = 4
     private val topicSubscriberCount = 16
     private val processor = Processor()
